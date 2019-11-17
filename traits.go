5a9030e49475f80ae579df8e52cd80142f85468f
reugn <reugpro@gmail.com>
2019-11-17 22:51:38 +0200

    add default initialization capability

--- traits.go
+++ traits.go
@@ -46,9 +46,45 @@ type validator interface {
 	setValidator(i interface{})
 }
 
+type _default interface {
+	initDefault(i interface{})
+}
+
+// Default trait for struct fields initialization
+// Will work for exported fields only
+type Default struct{}
+
+func (d *Default) initDefault(i interface{}) {
+	v := reflect.ValueOf(i).Elem()
+	t := reflect.TypeOf(i).Elem()
+	initStruct(t, v)
+}
+
+func initStruct(t reflect.Type, v reflect.Value) {
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		ft := t.Field(i)
+		switch ft.Type.Kind() {
+		case reflect.Map:
+			f.Set(reflect.MakeMap(ft.Type))
+		case reflect.Slice:
+			f.Set(reflect.MakeSlice(ft.Type, 0, 0))
+		case reflect.Chan:
+			f.Set(reflect.MakeChan(ft.Type, 0))
+		case reflect.Struct:
+			initStruct(ft.Type, f)
+		case reflect.Ptr:
+			fv := reflect.New(ft.Type.Elem())
+			initStruct(ft.Type.Elem(), fv.Elem())
+			f.Set(fv)
+		default:
+		}
+	}
+}
+
 var traitsPackage = reflect.TypeOf(Jsonify{}).PkgPath()
 
-// Jsonify trait provides ToJson capability
+// Jsonify trait provides ToJson marshalling method
 type Jsonify struct {
 	self interface{}
 }
@@ -63,7 +99,7 @@ func (js *Jsonify) ToJSON() (string, error) {
 	return string(b), err
 }
 
-// Stringify trait provides ToString capability
+// A Stringify trait provides improved ToString capability
 type Stringify struct {
 	self interface{}
 }
@@ -123,7 +159,7 @@ func deleteEmpty(s []string) []string {
 	return r
 }
 
-// Hash trait provides unique hash generators
+// A Hash trait provides unique hash generators
 type Hash struct {
 	self interface{}
 }
@@ -164,7 +200,7 @@ func (h *Hash) HashCode() uint32 {
 	return h32.Sum32()
 }
 
-// Validator trait provides struct fields validation capability
+// A Validator trait provides struct fields validation capability
 type Validator struct {
 	self interface{}
 }
@@ -178,7 +214,7 @@ func (v *Validator) Validate() (bool, error) {
 	return govalidator.ValidateStruct(v.self)
 }
 
-//Init traits capabilities for given object
+// Init traits capabilities for given object
 func Init(obj interface{}) error {
 	if o, ok := obj.(jsonify); ok {
 		o.setJsonify(obj)
@@ -194,6 +230,9 @@ func Init(obj interface{}) error {
 	}
 	if o, ok := obj.(bootstrap); ok {
 		o.Bootstrap()
+	}
+	if o, ok := obj.(_default); ok {
+		o.initDefault(obj)
 	}
 	if _, ok := obj.(finalize); ok {
 		var fin = func(f finalize) {
